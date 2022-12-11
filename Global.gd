2a2eb994497168ad75eb124e4e390c865da17bcf
Elizabeth Jiang <elijiang@iu.edu>
2022-12-10 22:35:46 -0500

    Finished

--- Global.gd
+++ Global.gd
@@ -1,6 +1,20 @@
 extends Node
 
+var timer = 0
+var score = 0
+
 
 func _input(_event):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+
+func update_score(s):
+	score += s
+	get_node("/root/Game/UI/Score").text = "Score: " + str(score)
+	
+func update_time():
+	var minutes = floor(timer / 60)
+	var seconds = timer % 60
+	var t = "Time: %02d" % minutes
+	t += ":%02d" % seconds
+	get_node("/root/Game/UI/Time").text = t
